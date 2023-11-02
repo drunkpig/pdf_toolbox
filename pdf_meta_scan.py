196fd781e268d2f8a6c2027af0bdd79f53fa6943
徐超 <xuchao@pjlab.org.cn>
2023-11-02 11:05:20 +0800

    重构pdf文件元信息扫描功能

--- pdf_meta_scan.py
+++ pdf_meta_scan.py
@@ -5,11 +5,9 @@
 
 import sys
 import click
-from libs.commons import  read_pdf
-import boto3, json
-from botocore.config import Config
+from libs.commons import read_pdf
+import json
 import fitz
-from loguru import logger
 
 
 def get_image_info(doc: fitz.Document) -> list:
@@ -20,29 +18,28 @@ def get_image_info(doc: fitz.Document) -> list:
         result.append(page_result)
         items = page.get_images()
         for img in items:
-            recs = page.get_image_rects(items[0], transform=True)
+            # 这里返回的是图片在page上的实际展示的大小。返回一个数组，每个元素第一部分是
+            recs = page.get_image_rects(img, transform=True)
             if recs:
-                for rec in recs:
-                    rec = rec[0]
-                    x0, y0, x1, y1 = int(rec[0]), int(rec[1]), int(rec[2]), int(rec[3])
-                    if (x0, y0, x1, y1) in dedup:  # 这里面会出现一些重复的bbox，无需重复出现，需要去掉
-                        continue
-                    dedup.add((x0, y0, x1, y1))
-                    width = x1 - x0
-                    height = y1 - y0
-                    page_result.append((width, height))
+                rec = recs[0][0]
+                x0, y0, x1, y1 = int(rec[0]), int(rec[1]), int(rec[2]), int(rec[3])
+                width = x1 - x0
+                height = y1 - y0
 
-                    # width = img['width']
-            # height = img['height']
-            # dpi_x, dpi_y = img['xres'], img['yres']
-            # page_result.append((width // dpi_x, height // dpi_y))
+                if (x0, y0, x1, y1) in dedup:  # 这里面会出现一些重复的bbox，无需重复出现，需要去掉
+                    continue
+                if all([width, height]):  # 长和宽任何一个都不能是0，否则这个图片不可见，没有实际意义
+                    continue
+                dedup.add((x0, y0, x1, y1))
+
+                page_result.append((width, height))
 
     return result
 
 
 def get_pdf_page_size_pts(doc):
     page_cnt = len(doc)
-    l = min(page_cnt, 5)
+    l: int = min(page_cnt, 5)
     # 取页面最大的宽和高度
     page_width_pts = 0
     page_height_pts = 0
@@ -50,7 +47,6 @@ def get_pdf_page_size_pts(doc):
         page = doc[i]
         page_rect = page.rect
         page_width_pts, page_height_pts = max(page_rect.width, page_width_pts), max(page_rect.height, page_height_pts)
-        # page_width_inch, page_height_inch = page_width_pts // 72, page_height_pts // 72
 
     return page_width_pts, page_height_pts
 
@@ -93,8 +89,6 @@ def main(s3_pdf_path: str, s3_profile: str):
         pdf_extractable_classfier(s3_pdf_path, file_content)
     except Exception as e:
         print(f"ERROR: {s3_pdf_path}, {e}", file=sys.stderr)
-        # logger.exception(e)
-        # exit(0)
 
 
 if __name__ == '__main__':
