5b0a99107d326086a5b8b28643fc25ec09b585f5
徐超 <xuchao@pjlab.org.cn>
2023-11-02 17:06:41 +0800

    优化识别扫描与非扫描版Pdf的方法

--- pdf_classfy_by_type.py
+++ pdf_classfy_by_type.py
@@ -1,10 +1,12 @@
 """
 根据利用meta_scan得到的结果，对pdf是否为文字版进行分类。
-分类条件：
-1. 页面大小不一致，波动
-2. 图片占比太大
-3. 文字过于稀疏，比如ppt
-4. 图片大小都基本一样，也就是方差很小：具体做法是每一page最大图片的方差小于1.5个标准差。
+定义标准：
+一、什么pdf会是文字pdf，只要满足以下任意一条
+  1. 随机抽取N页，如果有任何一页文字数目大于100
+  2. 只要存在一个页面，图片的数量为0
+二、什么是扫描版pdf，只要满足以下任意一条
+  1. ~~80%页面上的最大图大小一样并且面积超过页面面积0.6~~
+  2. 除匹配到文字版的其他所有
 
 """
 import click
@@ -12,6 +14,8 @@ import json
 import sys
 from loguru import logger
 import numpy as np
+
+TEXT_LEN_THRESHOLD = 100
 
 
 def mymax(alist: list):
@@ -21,51 +25,71 @@ def mymax(alist: list):
         return max(alist)
 
 
-def classify_by_area(page_width, page_height, img_sz_list, pdf_path):
+def classify_by_area(pdf_path, total_page: int, page_width, page_height, img_sz_list):
     """
-    如果是文本那么返回1，如果是图片那么返回0
+    80%页面上的最大图大小一样并且面积超过页面面积0.6则返回False，否则返回True
+    :param pdf_path:
+    :param total_page:
+    :param page_width:
+    :param page_height:
+    :param img_sz_list:
+    :return:
     """
-    total_pg = len(img_sz_list)
-    # 除掉没有任何图片的页面。
-    no_image_pages = [[[w, h] for w, h in img_info if w and h] for img_info in img_sz_list]
-    if len(no_image_pages) == 0:
-        return 1  # 说明没有图片，是一种文本pdf
-
-    # 判断方法：随机抽取3个页面，不足3个的取全部，判断这3个页面的图片面积占比是否大于0.8
-    select_page_cnt = 9
-    page_num = []
-    if total_pg <= select_page_cnt:
-        page_num = list(range(total_pg))
+    # 只要有一页没有图片，那么就是文字pdf
+    if any([len(img_sz) == 0 for img_sz in img_sz_list]):
+        return True
+    max_image_area_per_page = [mymax([(x1 - x0) * (y1 - y0) for x0, y0, x1, y1 in page_img_sz]) for page_img_sz in img_sz_list]
+    page_area = page_width * page_height
+    max_image_area_per_page = [area / page_area for area in max_image_area_per_page]
+    max_image_area_per_page = [area for area in max_image_area_per_page if area > 0.6]
+    if len(max_image_area_per_page) >= 0.8 * total_page:
+        return False
     else:
-        page_num = np.random.choice(total_pg, select_page_cnt, replace=False)
+        return True
 
-    image_page_cnt = 0
-    for i in page_num:
-        img_info = img_sz_list[i]
-        if len(img_info) > 0:
-            img_area = max([w * h for w, h in img_info])  # 只计算最大的图片面积
-            if img_area >= page_width * page_height * 0.8:
-                image_page_cnt += 1
 
-    result1 = int(image_page_cnt < select_page_cnt // 2)
+def classify_by_text_len(text_len_list: list, total_page: int):
+    """
+    随机抽取10%的页面，如果少于5个页面，那么就取全部页面。
+    查看页面上的文字长度，如果有任何一个页面的文字长度大于100，那么就是文字pdf
+    :param total_page:
+    :param text_len_list:
+    :return:
+    """
+    select_page_cnt = total_page // 10  # 选取10%的页面
+    if select_page_cnt < 5:
+        select_page_cnt = total_page
 
-    # 还有一种情况，这3个页面里图片的个数和最大图片大小一样，说明是扫描
-    # 只计算图片面积有时候阈值会有问题，因为有些图片是很小。因此另一种规则则对那些
-    # result2 = 0
-    # img_sz_list_selected = [img_sz_list[i] for i in page_num]
-    # image_per_page_cnt = [len(img_info) for img_info in img_sz_list_selected]
-    # image_area_max_per_page = [mymax([w*h for w,h in img_info]) for img_info in img_sz_list_selected]
+    page_num = np.random.choice(total_page, select_page_cnt, replace=False)
+    text_len_lst = [text_len_list[i] for i in page_num]
+    is_text_pdf = any([text_len > TEXT_LEN_THRESHOLD for text_len in text_len_lst])
+    return is_text_pdf
 
-    # if image_area_max_per_page[0] == 0:
-    #     result = 1
-    # elif len(set(image_per_page_cnt))==1 and len(set(image_area_max_per_page))==1:
-    #     result = 0
 
-    return result1
+def classify(pdf_path, total_page: int, page_width, page_height, img_sz_list: list, text_len_list: list):
+    """
+    这里的图片和页面长度单位是pts
+    :param total_page:
+    :param text_len_list:
+    :param page_width:
+    :param page_height:
+    :param img_sz_list:
+    :param pdf_path:
+    :return:
+    """
+    is_text_pdf_1 = classify_by_area(pdf_path, total_page, page_width, page_height, img_sz_list)
+    is_text_pdf_2 = classify_by_text_len(img_sz_list, total_page)
+    if all([is_text_pdf_1, is_text_pdf_2]):
+        return True
+    elif not any([is_text_pdf_1, is_text_pdf_2]):
+        return False
+    else:
+        print(f"WARNING: {pdf_path} is not classified by area and text_len", file=sys.stderr)
+        return False
 
 
 @click.command()
-@click.option("--json_file", type=str, help="pdf信息")
+@click.option("--json-file", type=str, help="pdf信息")
 def main(json_file):
     if json_file is None:
         print("json_file is None", file=sys.stderr)
@@ -76,11 +100,16 @@ def main(json_file):
                 if l.strip() == "":
                     continue
                 o = json.loads(l)
+                total_page = o["total_page"]
                 page_width = o["page_width_pts"]
                 page_height = o["page_height_pts"]
-                img_sz_list = o["image_area_pix_every_page"]
+                img_sz_list = o["image_info_per_page"]
+                text_len_list = o['text_len_per_page']
                 pdf_path = o['pdf_path']
-                tag = classify_by_area(page_width, page_height, img_sz_list, pdf_path)
+                is_encrypted = o['is_encrypted']
+                if is_encrypted:
+                    continue
+                tag = classify(pdf_path, total_page, page_width, page_height, img_sz_list, text_len_list)
                 o['is_text_pdf'] = tag
                 print(json.dumps(o, ensure_ascii=False))
     except Exception as e:
--- pdf_meta_scan.py
+++ pdf_meta_scan.py
@@ -11,9 +11,14 @@ import fitz
 
 
 def get_image_info(doc: fitz.Document) -> list:
+    """
+    返回每个页面里的图片的四元组，每个页面多个图片。
+    :param doc:
+    :return:
+    """
     result = []
     for page in doc:
-        page_result = []
+        page_result = []  # 存每个页面里的多张图四元组信息
         dedup = set()
         result.append(page_result)
         items = page.get_images()
@@ -32,12 +37,12 @@ def get_image_info(doc: fitz.Document) -> list:
                     continue
                 dedup.add((x0, y0, x1, y1))
 
-                page_result.append((width, height))
+                page_result.append((x0, y0, x1, y1))
 
     return result
 
 
-def get_pdf_page_size_pts(doc):
+def get_pdf_page_size_pts(doc: fitz.Document):
     page_cnt = len(doc)
     l: int = min(page_cnt, 5)
     # 取页面最大的宽和高度
@@ -51,8 +56,19 @@ def get_pdf_page_size_pts(doc):
     return page_width_pts, page_height_pts
 
 
-def pdf_extractable_classfier(s3_pdf_path: str, pdf_bytes: bytes):
+def get_pdf_textlen_per_page(doc: fitz.Document):
+    text_len_lst = []
+    for page in doc:
+        text_block = page.get_text("blocks")
+        text_block_len = sum([len(t[4]) for t in text_block])
+        text_len_lst.append(text_block_len)
+
+    return text_len_lst
+
+
+def pdf_meta_scan(s3_pdf_path: str, pdf_bytes: bytes):
     """
+    :param s3_pdf_path:
     :param pdf_bytes: pdf文件的二进制数据
     几个维度来评价：是否加密，是否需要密码，纸张大小，总页数，是否文字可提取
     """
@@ -61,7 +77,8 @@ def pdf_extractable_classfier(s3_pdf_path: str, pdf_bytes: bytes):
     is_encrypted = doc.is_encrypted
     total_page = len(doc)
     page_width_pts, page_height_pts = get_pdf_page_size_pts(doc)
-    image_area_pix_every_page = get_image_info(doc)
+    image_info_every_page = get_image_info(doc)
+    text_len_per_page = get_pdf_textlen_per_page(doc)
 
     # 最后输出一条json
     res = {
@@ -71,7 +88,8 @@ def pdf_extractable_classfier(s3_pdf_path: str, pdf_bytes: bytes):
         "total_page": total_page,
         "page_width_pts": int(page_width_pts),
         "page_height_pts": int(page_height_pts),
-        "image_area_pix_every_page": image_area_pix_every_page,
+        "image_info_per_page": image_info_every_page,
+        "text_len_per_page": text_len_per_page,
         "metadata": doc.metadata
     }
     print(json.dumps(res, ensure_ascii=False))
@@ -86,7 +104,7 @@ def main(s3_pdf_path: str, s3_profile: str):
     """
     try:
         file_content = read_pdf(s3_pdf_path, s3_profile)
-        pdf_extractable_classfier(s3_pdf_path, file_content)
+        pdf_meta_scan(s3_pdf_path, file_content)
     except Exception as e:
         print(f"ERROR: {s3_pdf_path}, {e}", file=sys.stderr)
 
